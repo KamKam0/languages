c0a859b0a2f716691235e45f8af699f1e6c2571b
KamKam0 <86293161+KamKam0@users.noreply.github.com>
2023-01-05 23:22:50 +0100

    Ajout typescript description

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 exports.version = require("./package.json").version
+/**
+ * 
+ * @param {string} iso 
+ * @returns {string}
+ */
 exports.getName = (iso) => {
     if(!iso) return 'error ISO'
     iso = String(iso).toLowerCase()
     if(!require("./datas.json")[iso]) return 'invalid ISO'
     else return require("./datas.json")[iso].name
 }
+/**
+ * 
+ * @param {string} iso 
+ * @returns {string}
+ */
 exports.getOriginalName = (iso) => {
     if(!iso) return 'error ISO'
     iso = String(iso).toLowerCase()
     if(!require("./datas.json")[iso]) return 'invalid ISO'
     else return require("./datas.json")[iso].nativeName
 }
+/**
+ * 
+ * @param {string} iso 
+ * @returns {object[]}
+ */
 exports.getAllNames = (iso) => {
     if(!iso) return 'error ISO'
     iso = String(iso).toLowerCase()
     if(!require("./datas.json")[iso]) return 'invalid ISO'
     else return require("./datas.json")[iso]
 }
+/**
+ * 
+ * @param {string} Language 
+ * @returns {object[]}
+ */
 exports.getISObyName = (Language) => {
     if(!Language) return 'error ISO'
     Language = String(Language).toLowerCase()
@@ -25,6 +45,11 @@ exports.getISObyName = (Language) => {
     if(!search) return 'invalid language'
     else return search[0]
 }
+/**
+ * 
+ * @param {string} Language 
+ * @returns {object[]}
+ */
 exports.getISObyFullName = (Language) => {
     if(!Language) return 'error ISO'
     Language = String(Language).toLowerCase()
